a9ba3ce3f608f49932bced4460861c5cf3d3fe30
JanisZhang <janis.zhang@liferay.com>
2024-04-05 21:17:35 +0800

    init commit

---
+++ python_edition3/name.py
@@ -0,0 +1,14 @@
+name = "aDA lovelace"
+print(name.title())
+print(name.lower())
+
+first_name = "janis"
+last_name = "zhang"
+full = f"{first_name} {last_name}"
+print(f"Hello, \t{full.title()}!")
+print(f"Hello, {full.title()}!")
+print(f"Hello, \n{full.title()}!")
+
+x = 'python   '
+print(x)
+print(x.rstrip())
---
+++ python_edition3/studyNotes.md
@@ -0,0 +1,10 @@
+## 变量和简单数据类型
+- 字符串:可以是单引号,也可以是双引号.  
+- f 字符串 (format)
+```
+first_name = "janis"
+last_name = "zhang"
+full = f"{first_name} {last_name}";
+print(f"Hello, {full.title()}!")
+```
+- 删除空白 rstrip()
