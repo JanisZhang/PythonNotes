230da249ec9f83c24ae80de7ed4686a7d78bb7dc
JanisZhang <janis.zhang@liferay.com>
2024-04-06 21:09:16 +0800

    update notes

--- python_edition3/name.py
+++ python_edition3/name.py
@@ -1,14 +1,60 @@
-name = "aDA lovelace"
-print(name.title())
-print(name.lower())
+# name = "aDA lovelace"
+# print(name.title())
+# print(name.lower())
+#
+# first_name = "janis"
+# last_name = "zhang"
+# full = f"{first_name} {last_name}"
+# print(f"Hello, \t{full.title()}!")
+# print(f"Hello, {full.title()}!")
+# print(f"Hello, \n{full.title()}!")
+#
+# x = '   python   '
+# print(x.strip())
+# print(x.rstrip())
+# print(x.lstrip())
 
-first_name = "janis"
-last_name = "zhang"
-full = f"{first_name} {last_name}"
-print(f"Hello, \t{full.title()}!")
-print(f"Hello, {full.title()}!")
-print(f"Hello, \n{full.title()}!")
+# nostarch_url = 'https://nostarch.com'
+# print(nostarch_url.removeprefix('https://'))
+# print(nostarch_url.removesuffix('.com'))
 
-x = 'python   '
-print(x)
-print(x.rstrip())
+# message = 'One of the python\'s strengths is ...'
+# print(message)
+
+# name cases
+# name = 'eric'
+# print(f'Hello, {name.title()},would you like to learn some Python today')
+#
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+#
+# message = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
+# print(message)
+# famous_name = 'Albert Einstein'
+# message = 'A person who never made a mistake never tried anything new.'
+# print(f'{famous_name} once said, "{message}"')
+#
+name = '\tJan\tis'
+# print(name)
+# print(name.lstrip())
+# print(name.rstrip())
+# print(name.strip())
+# print(name.replace('\t',''))
+# print(name.split())
+# a = ''
+# a = a.join(name.split())
+# print(a)
+
+# print(3/2)
+# print(3%2)
+# print(3**2)
+# print(3**3)
+
+# universe_age = 14_000_000_000.999_1234
+# print(universe_age)
+
+# MAX_CONNECTIONS = 5000
+
+import this
+print(this)
--- python_edition3/studyNotes.md
+++ python_edition3/studyNotes.md
@@ -7,4 +7,7 @@ last_name = "zhang"
 full = f"{first_name} {last_name}";
 print(f"Hello, {full.title()}!")
 ```
-- 删除空白 rstrip()
+- 删除空白 strip(),rstrip(),lstrip()
+- 删除前缀 removeprefix
+- 同时给多个变量赋值 x,y,z = 0,0,0
+- 常量, 将其变量名全大写
